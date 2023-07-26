f0c4c7aef64247736c09a08fd248a25f7eb53a71
Houda Jawad <houdajawad@gmail.com>
2023-07-26 10:20:22 -0700

    Add media queries for responsive design

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,19 +3,18 @@ import './Search.css';
 
 const Search = (props) => {
   return (
-    <div className='search-container'>
-      <form className="form">
-          <input
-            type="text"
-            name="search"
-            className="search-inp"
-            placeholder="Search"
-            onChange={(event) => {
-              props.filterArticles(event.target.value);
-            }}
-          />
-      </form>
-    </div>
+    <form className="form">
+      <input
+        type="text"
+        name="search"
+        className="search-inp"
+        placeholder="Search"
+        onChange={(event) => {
+          props.filterArticles(event.target.value);
+        }}
+      />
+   </form>
+
   );
 };
 
